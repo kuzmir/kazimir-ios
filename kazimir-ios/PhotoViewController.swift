3fc1aa832f38158a67b6f8567e4628f30f7aeb83
Krzysztof Cieplucha <krzysztof.cieplucha@gmail.com>
2015-05-17 20:36:37 +0200

    Make photo zoomable.

--- kazimir-ios/PhotoViewController.swift
+++ kazimir-ios/PhotoViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class PhotoViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var photo: Photo!
+    var imageView: UIImageView!
     
     @IBAction func tapGestureRecognized(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -20,7 +21,32 @@ class PhotoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = UIImage(data: photo.dataLarge)
+        let image = UIImage(data: photo.dataLarge)!
+        imageView = UIImageView(image: image)
+        scrollView.addSubview(imageView)
+        scrollView.contentSize = image.size
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let image = imageView.image!
+        let minHorizontalZoomScale = scrollView.frame.size.width / image.size.width
+        let minVerticalZoomScale = scrollView.frame.size.height / image.size.height
+        let minZoomScale = min(minHorizontalZoomScale, minVerticalZoomScale)
+        scrollView.minimumZoomScale = minZoomScale
+        scrollView.zoomScale = scrollView.minimumZoomScale
+        
+        let horizontalContentInset = (scrollView.frame.size.width - image.size.width * minZoomScale) / 2
+        let verticalContentInset = (scrollView.frame.size.height - image.size.height * minZoomScale) / 2
+        scrollView.contentInset = UIEdgeInsetsMake(verticalContentInset, horizontalContentInset, verticalContentInset, horizontalContentInset)
     }
 
+}
+
+extension PhotoViewController: UIScrollViewDelegate {
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
 }
